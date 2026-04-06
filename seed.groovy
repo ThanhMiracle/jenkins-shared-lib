2f7d954f440ac98e13b43ff48249216072c7df48
thanh <dtthanh2909@gmail.com>
2026-04-06 11:00:16 +0700

    update

--- seed.groovy
+++ seed.groovy
@@ -1,24 +1,21 @@
 services.each { svc ->
-    pipelineJob(svc.name) {
-        description("Pipeline for ${svc.name}")
-
-        parameters {
-            stringParam('BRANCH', 'main', 'Git branch to build')
+    multibranchPipelineJob("${svc.name}-mb") {
+        branchSources {
+            git {
+                id("${svc.name}-repo")
+                remote(repoUrl)
+                credentialsId(credsId)
+            }
         }
 
-        definition {
-            cpsScm {
-                scm {
-                    git {
-                        remote {
-                            url(repoUrl)
-                            credentials(credsId)
-                        }
-                        branch("\${BRANCH}")
-                    }
-                }
+        factory {
+            workflowBranchProjectFactory {
                 scriptPath(svc.scriptPath)
             }
+        }
+
+        triggers {
+            periodic(1)
         }
     }
 }
