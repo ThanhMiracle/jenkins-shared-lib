e70ab955c6b3d3c83ad99284ce379748ce4c2e9b
thanh <dtthanh2909@gmail.com>
2026-04-06 11:09:22 +0700

    update

--- seed.groovy
+++ seed.groovy
@@ -1,4 +1,10 @@
-services.each { svc ->
+def text = readFileFromWorkspace('envs/master.yml')
+def config = new groovy.yaml.YamlSlurper().parseText(text)
+
+def repoUrl = config.repoUrl
+def credsId = config.credentialsId
+
+(config.services ?: []).each { svc ->
     multibranchPipelineJob("${svc.name}-mb") {
         branchSources {
             git {
@@ -7,13 +13,11 @@ services.each { svc ->
                 credentialsId(credsId)
             }
         }
-
         factory {
             workflowBranchProjectFactory {
                 scriptPath(svc.scriptPath)
             }
         }
-
         triggers {
             periodic(1)
         }
